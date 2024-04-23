cbe4508d79c91fe035a81c09560ad66f07dc0a51
rishavConsoleLog21 <kumarrishavcool21@gmail.com>
2024-04-23 21:44:12 +0530

    + QuoteAPI

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 import { config } from "dotenv";
 import { Client, GatewayIntentBits } from "discord.js";
+import fetch from "node-fetch";
 
 config();
 
@@ -11,13 +12,30 @@ const client = new Client({
   ],
 });
 
+function getQuote() {
+  return fetch("https://zenquotes.io/api/random")
+    .then((res) => {
+      if (!res.ok) {
+        throw new Error("Failed to fetch");
+      }
+      return res.json();
+    })
+    .then((data) => {
+      return data[0]["q"] + " -" + data[0]["a"];
+    });
+}
+
 client.on("ready", () => {
   console.log(`Logged in as ${client.user.tag}`);
 });
 
-client.on("message", (msg) => {
-  if (msg.content === "ping") {
-    msg.reply("pong");
+client.on("messageCreate", (msg) => {
+  if (msg.author.bot) return;
+
+  if (msg.content === "$motivate") {
+    getQuote().then((quote) => {
+      msg.channel.send(quote);
+    });
   }
 });
 
