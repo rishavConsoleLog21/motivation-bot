98751930995f99cd8536d018033e3a9adbc0de8d
rishavConsoleLog21 <kumarrishavcool21@gmail.com>
2024-04-24 16:32:51 +0530

    + words and database

--- index.js
+++ index.js
@@ -1,8 +1,38 @@
 import { config } from "dotenv";
 import { Client, GatewayIntentBits } from "discord.js";
 import fetch from "node-fetch";
-
+import { sadWords, startEncouragements } from "./words.js";
+import Database from "@replit/database";
 config();
+
+/**
+ * Initiates Class.
+ * @param {String} dbUrl Custom database URL
+ */
+
+const db = new Database();
+
+db.get("encouragements").then((encouragements) => {
+  if (!encouragements || encouragements.length < 1) {
+    db.set("encouragements", startEncouragements);
+  }
+});
+
+function updateEncouragements(encouragingMessage) {
+  db.get("encouragements").then((encouragements) => {
+    encouragements.push([encouragingMessage]);
+    db.set("encouragements", encouragements);
+  });
+}
+
+function deleteEncouragement(index) {
+  db.get("encouragements").then((encouragements) => {
+    if (encouragements.length > index) {
+      encouragements.splice(index, 1);
+      db.set("encouragements", encouragements);
+    }
+  });
+}
 
 const client = new Client({
   intents: [
@@ -36,6 +66,25 @@ client.on("messageCreate", (msg) => {
     getQuote().then((quote) => {
       msg.channel.send(quote);
     });
+  }
+  if (sadWords.some((word) => msg.content.includes(word))) {
+    db.get("encouragements").then((encouragements) => {
+      const encouragement =
+        encouragements[Math.floor(Math.random() * encouragements.length)];
+      msg.reply(encouragement);
+    });
+  }
+
+  if (msg.content.startsWith("$new")) {
+    const encouragingMessage = msg.content.split("$new ")[1];
+    updateEncouragements(encouragingMessage);
+    msg.channel.send("New encouraging message added.");
+  }
+
+  if (msg.content.startsWith("$del")) {
+    const index = parseInt(msg.content.split("$del ")[1]);
+    deleteEncouragement(index);
+    msg.channel.send("Encouraging message deleted.");
   }
 });
 
